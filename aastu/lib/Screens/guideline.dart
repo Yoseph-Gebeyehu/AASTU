eef220992211cc9f3e6b23ee790a2ffc8b80bf66
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2022-11-15 10:06:31 -0800

    The guideline screen is modified.

--- aastu/lib/Screens/guideline.dart
+++ aastu/lib/Screens/guideline.dart
@@ -29,25 +29,45 @@ class Guidline extends StatelessWidget {
 
   When the login screen is diplayed, you must enter your user name and password. 
 
-  Refer the following exapmple table on how to use your id number and your Grandfather name as your user name and initial passwordrespectively.''',
+  Refer the following exapmple on how to use your id number and your Grandfather name as your user name and initial passwordrespectively.''',
                 style: Theme.of(context).textTheme.headline1,
               ),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 20, bottom: 20),
               child: Card(
+                // shadowColor: Color.fromARGB(255, 3, 47, 83),
+                // elevation: 10,
                 // color: Colors.transparent,
                 child: Text(
                   ''' Here is an example :-
-    Full name:- Yoseph Gebeyehu 
-    Grandfather Name:-Arega
-    Student ID:- ETS1179/11
+          If your              
+  Full name:- Yoseph Gebeyehu 
+  Student ID:- ets1179/11
 
-    Temporary password:-''',
+          Then your
+  User Name:- ets1179/11  
+  Password:- gebeyehu1234abcd#
+
+  * Notice that all characters in password must be in lower case!
+  
+  ''',
                   style: Theme.of(context).textTheme.headline1,
                 ),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                '''   As soon as you log in, the system will ask you to change your temporary password before accesing any resource that needs your identity.
+   Therefore,you have to fill the new password and other requested information properly.
+   The new password should not be less that 7 characters.
+  
+ * If you are unable to log in, please ask your concerned registrar office...because your ID number and/or father name may not be encoded correctly.
+   Remember what you fill in the biography form.''',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+            )
           ],
         ),
       ),
